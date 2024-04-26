82264e95ea8755a2c571565f0b0a20b612339c44
vyachnd <vyacheslav.nodata@yandex.ru>
2024-04-26 14:24:36 +0300

    feat: add settings variant, size, disabled for CELink

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-import { CEButton, CECheckbox, CEHeading, CEIcon, CEInputText, CELink } from './modules/custom-elements/init.js';
+import CreateElement from './modules/createElement.js';
+import { CEIcon, CELink } from './modules/custom-elements/init.js';
 
 
-const button = new CEButton(['Hello']);
-const checkbox = new CECheckbox(['Hello']);
-const heading = new CEHeading({ title: 'Hello' });
-const icon = new CEIcon('box');
-const inputText = new CEInputText([
-  new CEIcon('search'),
+const app = new CreateElement('div', [
+  new CELink(['Hello', new CEIcon('chevron_right')], {}, { href: 'https://www.github.com/vyachnd' }),
+  new CELink(['Hello', new CEIcon('chevron_right')], { variant: 'secondary' }, { href: 'https://www.github.com/vyachnd' }),
+  new CELink(['Hello', new CEIcon('chevron_right')], { variant: 'submit' }, { href: 'https://www.github.com/vyachnd' }),
+  new CELink(['Hello', new CEIcon('chevron_right')], { variant: 'warning' }, { href: 'https://www.github.com/vyachnd' }),
+  new CELink(['Hello', new CEIcon('chevron_right')], { variant: 'error' }, { href: 'https://www.github.com/vyachnd' }),
 ]);
-const link = new CELink([
-  'Hello',
-  new CEIcon('chevron_right'),
-], {}, { href: 'https://www.github.com/vyachnd' });
 
-button.mount(document.body, true);
-checkbox.mount(document.body, true);
-heading.mount(document.body, true);
-icon.mount(document.body, true);
-inputText.mount(document.body, true);
-link.mount(document.body, true);
+app.mount(document.body, true);
